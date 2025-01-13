5e2fe868f3be4d785cd6dd727c4a59ca1118f216
katsuta1104 <evening698564@gmail.com>
2025-01-13 22:30:01 +0900

    Create code.py

---
+++ algorithms/graph/BFS_List/code.py
@@ -0,0 +1,29 @@
+def bfs(graph):
+    #graphは隣接リスト表現
+    #頂点0からの最短距離を返す
+  
+    INF = 10**46
+    dist = [INF]*N
+    dist[0] = 0
+    
+    from collections import deque
+    
+    visited = set()
+    visited.add(0)
+    queue = deque()
+    
+    for a in graph[0]:
+        queue.append((a,1))
+        visited.add(a)
+    
+    while queue:
+        now_ver ,now_dis = queue.popleft()
+        
+        dist[now_ver] = now_dis
+        
+        for next in graph[now_ver]:
+            if next not in visited:
+                queue.append((next,now_dis+1))
+                visited.add(next)
+    
+    return dist
