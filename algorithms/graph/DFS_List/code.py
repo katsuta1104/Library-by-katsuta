ca05f793bbac0099537758277ac7bdb8192b9441
katsuta1104 <evening698564@gmail.com>
2025-01-13 22:33:54 +0900

    Update code.py

--- algorithms/graph/DFS_List/code.py
+++ algorithms/graph/DFS_List/code.py
@@ -3,10 +3,10 @@
 import sys
 sys.setrecursionlimit(2*10**5)
 visited = set()
-def dfs(graph,now):
+def dfs_list(graph,now):
     global visited
     visited.add(now)
     
     for next in graph[now]:
         if next not in visited:
-            dfs(graph,next)
+            dfs_list(graph,next)
